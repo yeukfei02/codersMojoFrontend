65afe028b1b0171febb7153cd144d7871f8277be
Donald Wu <yeukfei02@gmail.com>
2020-09-09 22:31:26 +0800

    refactor: 💡 add landing page

--- components/landingPage/LandingPage.tsx
+++ components/landingPage/LandingPage.tsx
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faTwitter, faMedium } from '@fortawesome/free-brands-svg-icons';
 
 function LandingPage(): JSX.Element {
-  return <div>LandingPage</div>;
+  const [email, setEmail] = useState('');
+
+  const weAreLaunchingSoonText = `<We are Launching Soon>`;
+
+  const handleEmailInputChange = (e: any) => {
+    if (e.target.value) {
+      setEmail(e.target.value);
+    }
+  };
+
+  const handleSubscribeButtonClick = () => {
+    console.log('email = ', email);
+  };
+
+  const handleInstagramClick = () => {
+    window.open('https://www.instagram.com/codersmojo');
+  };
+
+  const handleTwitterClick = () => {
+    window.open('https://www.google.com.hk');
+  };
+
+  const handleMediumClick = () => {
+    window.open('https://www.google.com.hk');
+  };
+
+  return (
+    <div>
+      <div className="text-center">Student Tech Ambassdor Applications are now open. Apply here</div>
+
+      <nav className="navbar navbar-light mx-5 my-3">
+        <a className="navbar-brand" href="#">
+          <img src="https://reactnative.dev/img/tiny_logo.png" width="80" height="80" alt="" loading="lazy" />
+        </a>
+        <div className="d-flex justify-content-end">
+          <div className="mx-5">Tech Blog</div>
+          <div>Become a Student Tech Ambassador</div>
+        </div>
+      </nav>
+
+      <h3 className="text-center font-weight-bold" style={{ marginTop: '3em' }}>
+        CodersMojo is an AI-based Peer-to-Peer Interactive Tech Interview Platform for Women
+      </h3>
+
+      <h1 className="text-center font-weight-bold weAreLaunchingSoonText" style={{ marginTop: '2em' }}>
+        {weAreLaunchingSoonText}
+      </h1>
+
+      <div className="text-center font-weight-bold" style={{ marginTop: '5em' }}>
+        Girls, let’s break the tech glass ceiling. Subscribe/ Join us for updates
+      </div>
+
+      <div className="mt-5 d-flex justify-content-center">
+        <div className="form-group w-50">
+          <input
+            type="text"
+            className="form-control"
+            id=""
+            placeholder="Email"
+            onChange={(e) => handleEmailInputChange(e)}
+          />
+        </div>
+      </div>
+
+      <div className="my-3 d-flex justify-content-center">
+        <button type="button" className="btn btn-primary btn-lg" onClick={() => handleSubscribeButtonClick()}>
+          Subscribe
+        </button>
+      </div>
+
+      <h5
+        className="d-flex justify-content-center"
+        style={{ position: 'absolute', bottom: 0, display: 'block', width: '100%' }}
+      >
+        Follow us :{' '}
+        <FontAwesomeIcon icon={faInstagram} className="mx-2 pointer" onClick={() => handleInstagramClick()} />{' '}
+        <FontAwesomeIcon icon={faTwitter} className="mx-2 pointer" onClick={() => handleTwitterClick()} />{' '}
+        <FontAwesomeIcon icon={faMedium} className="mx-2 pointer" onClick={() => handleMediumClick()} />
+      </h5>
+    </div>
+  );
 }
 
 export default LandingPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import Head from 'next/head';
 
-import MainView from '../components/mainView/MainView';
+import LandingPage from '../components/landingPage/LandingPage';
+// import MainView from '../components/mainView/MainView';
 
 function MainPage(): JSX.Element {
   return (
     <div>
-      <MainView />
+      <Head>
+        <script
+          src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+          integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+          integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+          integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <LandingPage />
     </div>
   );
 }
