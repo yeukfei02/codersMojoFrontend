cc9f4a60ad0cc59d5f6f22ab1999a2ff7f1f8ad4
Donald Wu <yeukfei02@gmail.com>
2020-09-11 21:58:08 +0800

    refactor: 💡 add missing head tag

--- components/techBlog/TechBlog.tsx
+++ components/techBlog/TechBlog.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
 
 function TechBlog(): JSX.Element {
-  return <div>TechBlog</div>;
+  return (
+    <div>
+      <Head>
+        <title>CodersMojo</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      TechBlog
+    </div>
+  );
 }
 
 export default TechBlog;
