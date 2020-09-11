a03cbd938b6807c2271672858a4c53b44daecaf4
Donald Wu <yeukfei02@gmail.com>
2020-09-12 00:18:42 +0800

    refactor: 💡 add navbar component

---
+++ components/navBar/NavBar.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import Head from 'next/head';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import { pink } from '@material-ui/core/colors';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+    },
+    pink: {
+      color: theme.palette.getContrastText(pink[500]),
+      backgroundColor: pink[500],
+    },
+  }),
+);
+
+function NavBar(): JSX.Element {
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleSettings = () => {
+    setAnchorEl(null);
+  };
+
+  const handleReportABug = () => {
+    setAnchorEl(null);
+  };
+
+  const handleYourAccount = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    setAnchorEl(null);
+
+    localStorage.clear();
+    window.location.reload();
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>CodersMojo</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              <a className="navbar-brand" href="/">
+                <img src="/logo_transparent.png" width="80" height="80" alt="" loading="lazy" />
+              </a>
+            </Typography>
+            <Avatar
+              className={`${classes.pink} pointer`}
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+            >
+              N
+            </Avatar>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleSettings}>Settings</MenuItem>
+              <MenuItem onClick={handleReportABug}>Report a Bug</MenuItem>
+              <MenuItem onClick={handleYourAccount}>Your Account</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
