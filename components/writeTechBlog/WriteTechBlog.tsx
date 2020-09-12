c33f86b9635c9f374cc585b01cbc7ee4c890cff6
Donald Wu <yeukfei02@gmail.com>
2020-09-12 19:12:29 +0800

    refactor: 💡 add write tech blog page

---
+++ components/writeTechBlog/WriteTechBlog.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+function WriteTechBlog(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>CodersMojo</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      </Head>
+      WriteTechBlog
+    </div>
+  );
+}
+
+export default WriteTechBlog;
---
+++ pages/write-tech-blog.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WriteTechBlog from '../components/writeTechBlog/WriteTechBlog';
+
+function WriteTechBlogPage(): JSX.Element {
+  return (
+    <div>
+      <WriteTechBlog />
+    </div>
+  );
+}
+
+export default WriteTechBlogPage;
