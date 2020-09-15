6ff03c87aae2389d4252f947baa30afbc02740a7
Donald Wu <yeukfei02@gmail.com>
2020-09-15 16:37:48 +0800

    refactor: 💡 update discussion board page

---
+++ components/createPosts/CreatePosts.tsx
@@ -0,0 +1,242 @@
+import React, { useEffect, useState } from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Select from 'react-select';
+
+import NextHead from '../nextHead/NextHead';
+import CustomSnackBar from '../customSnackBar/CustomSnackBar';
+
+// const useStyles = makeStyles({
+//   root: {
+//     width: 600,
+//     padding: 35,
+//   },
+// });
+
+const selectStyles = {
+  container: (base: any, state: any) => ({
+    ...base,
+    opacity: state.isDisabled ? '.5' : '1',
+    backgroundColor: 'transparent',
+    zIndex: '999',
+  }),
+};
+
+function CreatePosts(props: any): JSX.Element {
+  // const classes = useStyles();
+
+  const [selectedTagList, setSelectedTagList] = useState<any[]>([]);
+  const [selectedTag, setSelectedTag] = useState<any>(null);
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [tag, setTag] = useState('');
+  const [users_id, setUsers_id] = useState(0);
+
+  const [snackBarStatus, setSnackBarStatus] = useState(false);
+  const [snackBarType, setSnackBarType] = useState('success');
+  const [snackBarMessage, setSnackBarMessage] = useState('');
+
+  useEffect(() => {
+    getSelectedTagList();
+    getUsersId();
+  }, []);
+
+  const getSelectedTagList = () => {
+    const tagList = [
+      {
+        label: 'Career',
+        value: 'Career',
+      },
+      {
+        label: 'Jobs',
+        value: 'Jobs',
+      },
+      {
+        label: 'Success Story',
+        value: 'Success Story',
+      },
+      {
+        label: 'Women Investor',
+        value: 'Women Investor',
+      },
+      {
+        label: 'Job Board',
+        value: 'Job Board',
+      },
+      {
+        label: 'Tech',
+        value: 'Tech',
+      },
+      {
+        label: 'Salary Negotiation',
+        value: 'Salary Negotiation',
+      },
+      {
+        label: 'Compensation',
+        value: 'Compensation',
+      },
+      {
+        label: 'Interview Experience',
+        value: 'Interview Experience',
+      },
+      {
+        label: 'Career Growth',
+        value: 'Career Growth',
+      },
+      {
+        label: 'Students',
+        value: 'Students',
+      },
+      {
+        label: 'Introduce Yourself',
+        value: 'Introduce Yourself',
+      },
+    ];
+    setSelectedTagList(tagList);
+  };
+
+  const getUsersId = () => {
+    const users_id = localStorage.getItem('usersId');
+    if (users_id) {
+      const userIdInt = parseInt(users_id, 10);
+      setUsers_id(userIdInt);
+    }
+  };
+
+  const handleTitleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) {
+      setTitle(e.target.value);
+    }
+  };
+
+  const handleDescriptionInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (e.target.value) {
+      setDescription(e.target.value);
+    }
+  };
+
+  const handleTagDropdownChange = (selectedTag: any) => {
+    if (selectedTag) {
+      setSelectedTag(selectedTag);
+      setTag(selectedTag.value);
+    } else {
+      setSelectedTag(null);
+      setTag('');
+    }
+  };
+
+  const handleSubmitButtonClick = (title: string, description: string, tag: string, users_id: number) => {
+    if (title && description && tag && users_id) {
+      createPosts(title, description, tag, users_id);
+    }
+  };
+
+  const createPosts = async (title: string, description: string, tag: string, users_id: number) => {
+    const token = localStorage.getItem('token');
+
+    const response = await fetch(`/api/posts/create-posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        description: description,
+        tag: tag,
+        users_id: users_id,
+        token: token,
+      }),
+    });
+    if (response) {
+      const responseData = await response.json();
+      console.log('response status = ', response.status);
+      console.log('responseData = ', responseData);
+
+      if (response.status === 200) {
+        setSnackBarStatus(true);
+        setSnackBarType('success');
+        setSnackBarMessage('create post success');
+
+        setTimeout(() => {
+          props.discussionBoardClick();
+        }, 1000);
+      } else {
+        setSnackBarStatus(true);
+        setSnackBarType('error');
+        setSnackBarMessage('create post error');
+      }
+    }
+  };
+
+  const handleCloseSnackBar = () => {
+    setSnackBarStatus(false);
+  };
+
+  const handleBackClick = () => {
+    props.discussionBoardClick();
+  };
+
+  return (
+    <div style={{ margin: '5em auto' }}>
+      <NextHead />
+
+      <div className="container d-flex justify-content-center">
+        <Card style={{ width: window.innerWidth > 600 ? 600 : 370, padding: '3em' }} variant="outlined">
+          <div className="my-2 d-flex justify-content-center">
+            <img src="/logo.png" width="200" height="65" alt="" loading="lazy" />
+          </div>
+
+          <h4 className="text-center my-5 font-weight-bold">Create post</h4>
+
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input type="text" className="form-control" id="title" onChange={(e) => handleTitleInputChange(e)} />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="exampleFormControlTextarea1">Description</label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows={5}
+              onChange={(e) => handleDescriptionInputChange(e)}
+            ></textarea>
+          </div>
+
+          <label>Tag</label>
+          <div className="mt-1 mb-3">
+            <Select
+              styles={selectStyles}
+              placeholder={'Select tag'}
+              value={selectedTag}
+              onChange={handleTagDropdownChange}
+              options={selectedTagList}
+              isClearable={true}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-success btn-lg w-100 my-3"
+            onClick={() => handleSubmitButtonClick(title, description, tag, users_id)}
+          >
+            Submit
+          </button>
+
+          <div className="d-flex justify-content-center mt-5">
+            <span className="pointer hover-item" onClick={() => handleBackClick()}>
+              Back
+            </span>
+          </div>
+        </Card>
+      </div>
+
+      <CustomSnackBar
+        snackBarStatus={snackBarStatus}
+        snackBarType={snackBarType}
+        snackBarMessage={snackBarMessage}
+        closeSnackBar={() => handleCloseSnackBar()}
+      />
+    </div>
+  );
+}
+
+export default CreatePosts;
--- components/discussionBoard/DiscussionBoard.tsx
+++ components/discussionBoard/DiscussionBoard.tsx
@@ -1,25 +1,125 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import NextHead from '../nextHead/NextHead';
 
-function DiscussionBoard(): JSX.Element {
-  const handleCreatePostButtonClick = () => {
-    console.log(123);
+function DiscussionBoard(props: any): JSX.Element {
+  const [postsList, setPostsList] = useState<any[]>([]);
+
+  const [filterText, setFilterText] = useState('');
+
+  useEffect(() => {
+    getPostsList();
+  }, []);
+
+  useEffect(() => {
+    if (filterText) {
+      getPostsList(filterText);
+    }
+  }, [filterText]);
+
+  const getPostsList = async (tag?: string) => {
+    const token = localStorage.getItem('token');
+
+    let response = null;
+    if (!tag) {
+      if (token) {
+        const queryString = new URLSearchParams({
+          token: token,
+        });
+        response = await fetch(`/api/posts?${queryString}`);
+      }
+    } else {
+      if (token) {
+        const queryString = new URLSearchParams({
+          tag: tag,
+          token: token,
+        });
+        response = await fetch(`/api/posts?${queryString}`);
+      }
+    }
+
+    if (response) {
+      const responseData = await response.json();
+      console.log('response status = ', response.status);
+      console.log('responseData = ', responseData);
+
+      if (responseData) {
+        setPostsList(responseData.result.result);
+      }
+    }
   };
 
-  // const renderPostsList = (postList: any[]) => {
-  //   let postListView = null;
+  const renderPostsList = (postsList: any[]) => {
+    let postListView = null;
 
-  //   if (postList) {
-  //     postListView = (
-  //       <div>
+    if (postsList) {
+      postListView = postsList.map((item: any, i: number) => {
+        const number = i + 1;
+        const title = item.title;
+        const description = item.description;
+        const tag = item.tag;
 
-  //       </div>
-  //     );
-  //   }
+        return (
+          <div key={i} className="my-3">
+            <div className="card">
+              <div className="card-body">
+                <div className="mt-2 mb-3" style={{ fontSize: '1.2em' }}>
+                  <b>Discussion Thread {number}</b> - {title}
+                </div>
+                <div className="my-3" style={{ fontSize: '1.2em' }}>
+                  <b>Answer</b> - {description}
+                </div>
+                <div>{renderTag(tag)}</div>
+              </div>
+            </div>
+          </div>
+        );
+      });
+    }
 
-  //   return postListView;
-  // };
+    return postListView;
+  };
+
+  const renderTag = (tag: string) => {
+    let tagView = null;
+
+    switch (tag) {
+      case 'Career':
+      case 'Jobs':
+      case 'Success Story':
+        tagView = <span className="badge badge-pill p-2 badge-primary"># {tag}</span>;
+        break;
+      case 'Women Investor':
+      case 'Job Board':
+        tagView = <span className="badge badge-pill p-2 badge-secondary"># {tag}</span>;
+        break;
+      case 'Tech':
+      case 'Salary Negotiation':
+        tagView = <span className="badge badge-pill p-2 badge-success"># {tag}</span>;
+        break;
+      case 'Compensation':
+        tagView = <span className="badge badge-pill p-2 badge-danger"># {tag}</span>;
+        break;
+      case 'Interview Experience':
+        tagView = <span className="badge badge-pill p-2 badge-warning"># {tag}</span>;
+        break;
+      case 'Career Growth':
+        tagView = <span className="badge badge-pill p-2 badge-info"># {tag}</span>;
+        break;
+      case 'Students':
+      case 'Introduce Yourself':
+        tagView = <span className="badge badge-pill p-2 badge-dark"># {tag}</span>;
+        break;
+      default:
+        break;
+    }
+
+    return tagView;
+  };
+
+  const handleFilterTextClick = (filterText: string) => {
+    setFilterText(filterText);
+  };
 
   return (
     <div>
@@ -30,41 +130,65 @@ function DiscussionBoard(): JSX.Element {
           <div className="col-sm-4">
             <div className="card">
               <div className="card-body">
-                <h6 className="my-4">
-                  <span className="pointer hover-item">Career</span>
+                <h6 className="mt-3 mb-4">
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Career')}>
+                    Career
+                  </span>
                 </h6>
                 <h6 className="my-4">
-                  <span className="pointer hover-item">Jobs</span>
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Jobs')}>
+                    Jobs
+                  </span>
                 </h6>
                 <h6 className="my-4">
-                  <span className="pointer hover-item">Success Story</span>
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Success Story')}>
+                    Success Story
+                  </span>
                 </h6>
                 <h6 className="my-4">
-                  <span className="pointer hover-item">Women Investor</span>
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Women Investor')}>
+                    Women Investor
+                  </span>
                 </h6>
                 <h6 className="my-4">
-                  <span className="pointer hover-item">Job Board</span>
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Job Board')}>
+                    Job Board
+                  </span>
                 </h6>
                 <h6 className="my-4">
-                  <span className="pointer hover-item">Tech</span>
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Tech')}>
+                    Tech
+                  </span>
                 </h6>
                 <h6 className="my-4">
-                  <span className="pointer hover-item">Salary Negotiation</span>
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Salary Negotiation')}>
+                    Salary Negotiation
+                  </span>
                 </h6>
                 <h6 className="my-4">
-                  <span className="pointer hover-item">Compensation</span>
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Compensation')}>
+                    Compensation
+                  </span>
                 </h6>
                 <h6 className="my-4">
-                  <span className="pointer hover-item">Interview Experience</span>
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Interview Experience')}>
+                    Interview Experience
+                  </span>
                 </h6>
                 <h6 className="my-4">
-                  <span className="pointer hover-item">Career Growth</span>
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Career Growth')}>
+                    Career Growth
+                  </span>
                 </h6>
                 <h6 className="my-4">
-                  <span className="pointer hover-item">Students</span>
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Students')}>
+                    Students
+                  </span>
                 </h6>
                 <h6 className="my-4">
-                  <span className="pointer hover-item">Introduce Yourself</span>
+                  <span className="pointer hover-item" onClick={() => handleFilterTextClick('Introduce Yourself')}>
+                    Introduce Yourself
+                  </span>
                 </h6>
               </div>
             </div>
@@ -75,13 +199,13 @@ function DiscussionBoard(): JSX.Element {
                 <div style={{ display: 'flex', flexDirection: 'row' }}>
                   <h5 className="my-2">CodersMojo Discussion Board</h5>
                   <div style={{ marginLeft: 'auto' }}>
-                    <button type="submit" className="btn btn-primary" onClick={() => handleCreatePostButtonClick()}>
+                    <button type="submit" className="btn btn-primary" onClick={() => props.postsClick()}>
                       Create post
                     </button>
                   </div>
                 </div>
 
-                {/* {renderPostsList(postList)} */}
+                {renderPostsList(postsList)}
               </div>
             </div>
           </div>
