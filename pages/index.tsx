b86529ef9229cd2f11a649f4b02fcc952882518b
Donald Wu <yeukfei02@gmail.com>
2020-09-17 21:12:41 +0800

    feat: 🎸 add firebase messaging

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/messaging';
+
+import { getFirebaseConfig } from '../common/common';
 
 import LandingPage from '../components/landingPage/LandingPage';
 import MainView from '../components/mainView/MainView';
@@ -16,12 +21,43 @@ const theme = createMuiTheme({
   },
 });
 
+// firebase
+let messaging: any = null;
+if (!firebase.apps.length) {
+  const firebaseConfig = getFirebaseConfig();
+  firebase.initializeApp(firebaseConfig);
+
+  messaging = firebase.messaging();
+  messaging.usePublicVapidKey(process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_VAPID_KEY as string);
+}
+
 function MainPage(): JSX.Element {
+  const [users_id, setUsers_id] = useState('');
   const [token, setToken] = useState('');
 
+  const [firebaseCurrentToken, setFirebaseCurrentToken] = useState('');
+  const [firebaseRefreshedToken, setFirebaseRefreshedToken] = useState('');
+
   useEffect(() => {
+    getUsers_id();
     getToken();
+    getFirebaseCurrentToken();
+    getFirebaseRefreshedToken();
   }, []);
+
+  useEffect(() => {
+    if ((firebaseCurrentToken || firebaseRefreshedToken) && users_id) {
+      addTokenToFirebaseDetails(firebaseCurrentToken, firebaseRefreshedToken);
+      subscribeTopic();
+    }
+  }, [firebaseCurrentToken, firebaseRefreshedToken, users_id]);
+
+  const getUsers_id = () => {
+    const users_id = localStorage.getItem('usersId');
+    if (users_id) {
+      setUsers_id(users_id);
+    }
+  };
 
   const getToken = () => {
     const token = localStorage.getItem('token');
@@ -30,10 +66,79 @@ function MainPage(): JSX.Element {
     }
   };
 
-  const renderDiv = (token: string) => {
+  const getFirebaseCurrentToken = async () => {
+    try {
+      const currentToken = await messaging.getToken();
+      if (currentToken) {
+        console.log('currentToken = ', currentToken);
+        localStorage.setItem('firebaseCurrentToken', currentToken);
+        setFirebaseCurrentToken(currentToken);
+      } else {
+        console.log('No Instance ID token available. Request permission to generate one.');
+      }
+    } catch (e) {
+      console.log('An error occurred while retrieving token. ', e.message);
+    }
+  };
+
+  const getFirebaseRefreshedToken = async () => {
+    try {
+      messaging.onTokenRefresh(async () => {
+        const refreshedToken = await messaging.getToken();
+        if (refreshedToken) {
+          console.log('refreshedToken = ', refreshedToken);
+          localStorage.setItem('firebaseRefreshedToken', refreshedToken);
+          setFirebaseRefreshedToken(refreshedToken);
+        }
+      });
+    } catch (e) {
+      console.log('Unable to retrieve refreshed token. ', e.message);
+    }
+  };
+
+  const addTokenToFirebaseDetails = async (firebaseCurrentToken: string, firebaseRefreshedToken: string) => {
+    if (users_id) {
+      const response = await fetch(`/api/firebase/add-token-to-firebase-details`, {
+        method: 'POST',
+        body: JSON.stringify({
+          currentToken: firebaseCurrentToken,
+          refreshedToken: firebaseRefreshedToken,
+          users_id: users_id,
+        }),
+      });
+      if (response) {
+        const responseData = await response.json();
+        console.log('response.status = ', response.status);
+        console.log('responseData = ', responseData);
+      }
+    }
+  };
+
+  const subscribeTopic = async () => {
+    if (users_id) {
+      const response = await fetch(`/api/firebase/subscribe-topic`, {
+        method: 'POST',
+        body: JSON.stringify({
+          registrationTokenList: [firebaseCurrentToken],
+          topic: 'all',
+          users_id: users_id,
+        }),
+      });
+      if (response) {
+        const responseData = await response.json();
+        console.log('response.status = ', response.status);
+        console.log('responseData = ', responseData);
+
+        if (response.status === 200) {
+        }
+      }
+    }
+  };
+
+  const renderDiv = (users_id: string, token: string) => {
     let resultDiv = <LandingPage />;
 
-    if (token) {
+    if (users_id && token) {
       resultDiv = <MainView />;
     }
 
@@ -62,7 +167,7 @@ function MainPage(): JSX.Element {
           crossOrigin="anonymous"
         ></script>
       </Head>
-      {renderDiv(token)}
+      {renderDiv(users_id, token)}
     </MuiThemeProvider>
   );
 }
