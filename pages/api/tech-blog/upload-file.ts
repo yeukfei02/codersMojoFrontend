0aba80798fb0de197e9bdc2afe6d411d5f98f2ac
Donald Wu <yeukfei02@gmail.com>
2020-09-18 22:41:36 +0800

    refactor: 💡 fix file upload

---
+++ pages/api/tech-blog/upload-file.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+
+import { getRootUrl } from '../../../common/common';
+
+const ROOT_URL = getRootUrl();
+
+export default async (_req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const reqBody = JSON.parse(_req.body);
+    const response = await axios({
+      method: 'post',
+      url: `${ROOT_URL}/tech-blog/upload-file`,
+      data: reqBody.bodyFormData,
+      headers: { Authorization: `Bearer ${reqBody.token}`, 'Content-Type': 'multipart/form-data' },
+    });
+
+    if (response) {
+      const responseData = response.data;
+      res.status(200).json({
+        result: responseData,
+      });
+    }
+  } catch (e) {
+    res.status(400).json({
+      message: e.message,
+    });
+  }
+};
