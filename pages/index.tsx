fd1f9fa2b074eb63d3f4cdded3d7285cce393657
Donald Wu <yeukfei02@gmail.com>
2020-09-28 16:46:31 +0800

    refactor: 💡 fix code

--- pages/index.tsx
+++ pages/index.tsx
@@ -182,11 +182,11 @@ function MainPage(): JSX.Element {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-179040882-1');
-        `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-179040882-1');
+            `,
           }}
         />
       </Head>
